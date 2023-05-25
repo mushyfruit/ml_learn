651df9ff2a1b259ea18bcf0ecb64b8e078d8fa86
Andrew-WindowsPC-Laptop <andrew.boyles11@gmail.com>
2023-05-24 22:44:42 -0700

    Add displaycallback

--- unet_practice/main.py
+++ unet_practice/main.py
@@ -9,6 +9,38 @@ from unet_model import unet
 dataset, info = tfds.load('oxford_iiit_pet:3.*.*', with_info=True)
 
 
+class DisplayCallback(tf.keras.callbacks.Callback):
+    def on_epoch_end(self, epoch, logs=None):
+        show_predictions()
+        print('\nSample Prediction after epoch {}\n'.format(epoch + 1))
+
+
+def display(display_list):
+    plt.figure(figsize=(15, 15))
+
+    title = ['Input Image', 'True Mask', 'Predicted Mask']
+
+    for i in range(len(display_list)):
+        plt.subplot(1, len(display_list), i + 1)
+        plt.title(title[i])
+        plt.imshow(tf.keras.preprocessing.image.array_to_img(display_list[i]))
+        plt.axis('off')
+    plt.show()
+
+
+def create_mask(pred_mask):
+    pred_mask = tf.argmax(pred_mask, axis=-1)
+    pred_mask = pred_mask[..., tf.newaxis]
+    return pred_mask[0]
+
+
+def show_predictions(data=None, num=1):
+    if data:
+        for image, mask in data.take(num):
+            pred_mask = model.predict(image)
+            display([image[0], mask[0], create_mask(pred_mask)])
+
+
 def normalize(input_image, input_mask):
     input_image = tf.cast(input_image, tf.float32) / 255.0
     input_mask -= 1
@@ -33,7 +65,7 @@ def load_image(datapoint):
 
 # Preprocessing
 train_images = dataset['train'].map(load_image,
-                             num_parallel_calls=tf.data.AUTOTUNE)
+                                    num_parallel_calls=tf.data.AUTOTUNE)
 test_images = dataset['test'].map(load_image,
                                   num_parallel_calls=tf.data.AUTOTUNE)
 
@@ -60,9 +92,10 @@ for layer in base_model.layers:
 # base_model.summary()
 EPOCHS = 1
 VAL_SUBSPLITS = 5
-VALIDATION_STEPS = info.splits['test'].num_examples//BATCH_SIZE//VAL_SUBSPLITS
+VALIDATION_STEPS = info.splits['test'].num_examples // BATCH_SIZE // VAL_SUBSPLITS
 
 model = unet(base_model)
+# tf.keras.utils.plot_model(model, show_shapes=True)
 model_history = model.fit(train_batches,
                           epochs=EPOCHS,
                           steps_per_epoch=STEPS_PER_EPOCH,
